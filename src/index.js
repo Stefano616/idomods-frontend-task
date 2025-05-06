f31203ea5dcf7c3b5cb7f184e88e3a0bc7371ee9
Stefano Rocchetti <stefanorocchetti4@gmail.com>
2025-05-06 23:23:08 +0200

    Fix page scroll on burger menu open
    Prevent scroll event did not work.
    Change div to button for better semantic and accessibility.

--- src/index.js
+++ src/index.js
@@ -8,9 +8,28 @@ const navClose = document.querySelector(".navigation__menu-close");
 const nav = document.querySelector("nav");
 const overlayBg = document.querySelector("#overlay");
 
-navOpen.addEventListener("click", () => nav.classList.add("visible"));
-navClose.addEventListener("click", () => nav.classList.remove("visible"));
-overlayBg.addEventListener("click", () => nav.classList.remove("visible"));
+navOpen.addEventListener("click", () => {
+  nav.classList.add("visible");
+  navClose.focus({ focusVisible: false });
+});
+navClose.addEventListener("click", () => {
+  nav.classList.remove("visible");
+});
+overlayBg.addEventListener("click", (e) => {
+  nav.classList.remove("visible");
+});
+["wheel", "touchmove", "keyup", "keydown"].forEach((e) => {
+  nav.addEventListener(e, preventScroll);
+  overlayBg.addEventListener(e, preventScroll);
+});
+function preventScroll(e) {
+  // if Tab is pressed the event is not stopped.
+  if (e.keyCode === 9) {
+    return false;
+  }
+  e.preventDefault();
+  e.stopPropagation();
+}
 
 const links = document.querySelectorAll(".nav__link");
 
